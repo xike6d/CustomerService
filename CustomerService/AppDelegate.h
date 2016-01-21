0e637bf322e5fc46e8b76b973267e4ae1f6b8f97
秦莉明 <mianyang621000@hotmail.com>
2016-01-21 15:29:46 +0800

    Initial Commit

---
+++ CustomerService/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomerService
+//
+//  Created by 刘荻 on 16/1/21.
+//  Copyright © 2016年 刘荻. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomerService/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomerService
+//
+//  Created by 刘荻 on 16/1/21.
+//  Copyright © 2016年 刘荻. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
